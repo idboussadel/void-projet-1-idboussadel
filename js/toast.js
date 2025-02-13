b4b0abd1b66d68cd3c7d9212051ce13dd327c431
idboussadel <idbouid@gmail.com>
2025-02-13 15:38:05 +0100

    added toast

---
+++ js/toast.js
@@ -0,0 +1,33 @@
+class Toast extends HTMLElement {
+  static get observedAttributes() {
+    return ["message", "type"];
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === "message" && oldValue !== newValue) {
+      this.render();
+    }
+  }
+
+  render() {
+    const message = this.getAttribute("message") || "Error oops...";
+    const type = this.getAttribute("type") || "error";
+
+    this.innerHTML = `
+          <div id="toast" class="cursor-pointer flex gap-4 bg-blue-800 items-center justify-center px-4 shadow-sm py-3 min-w-[200px] border rounded fixed top-4 left-1/2 -translate-x-1/2 bg-white transform -translate-y-[100px] duration-500 ease">
+                ${
+                  type === "success"
+                    ? '<img id="check-btn" src="./assets/check.svg" class="h-[24px] w-[24px]" alt="check icon">'
+                    : '<img id="error-btn" src="./assets/close-icon.svg" class="h-[24px] w-[24px]" alt="check icon"/>'
+                }
+                <p class="text-blue-800">${message}</p>
+          </div>
+      `;
+  }
+}
+
+customElements.define("custom-toast", Toast);
--- js/validate-form.js
+++ js/validate-form.js
@@ -13,10 +13,14 @@ const validate = (value, inputId, regex, errorMsg) => {
       `<p class="text-red-400">${errorMsg}</p>`
     );
     input.classList.add("border-red-400");
+    return false;
   }
+  input.classList.remove("border-red-400");
+  return true;
 };
 
 const submitContact = (e) => {
+  e.preventDefault();
   const inputNom = document.getElementById("nom");
   const inputPrenom = document.getElementById("prenom");
   const inputEmail = document.getElementById("email");
@@ -24,25 +28,42 @@ const submitContact = (e) => {
   const inputMessage = document.getElementById("message");
   const inputConditions = document.getElementById("conditions");
 
+  let isValid = true;
+
   const optionMmeCivilite = document.getElementById("Mme");
   const optionMCivilite = document.getElementById("M");
 
   // input validation
-  validate(inputNom.value, "nom", "^[A-Za-zÀ-ÿs]+$", "Name is required");
-  validate(
+  isValid &= validate(
+    inputNom.value,
+    "nom",
+    "^[A-Za-zÀ-ÿs]+$",
+    "Name is required"
+  );
+
+  isValid &= validate(
     inputPrenom.value,
     "prenom",
     "^[A-Za-zÀ-ÿs]+$",
     "Prenom is required"
   );
-  validate(inputEmail.value, "email", "^[A-Za-zÀ-ÿs]+$", "Enter a valid email");
-  validate(
+  console.log(isValid);
+
+  isValid &= validate(
+    inputEmail.value,
+    "email",
+    "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$",
+    "Enter a valid email"
+  );
+
+  isValid &= validate(
     inputTele.value,
     "tele",
     "^(\\+?\\d{1,3}[-\\s]?)?(\\(?\\d{3}\\)?[-\\s]?)?\\d{7,15}$",
     "Enter a valid phone number"
   );
-  validate(
+
+  isValid &= validate(
     inputMessage.value,
     "message",
     "^[A-Za-zÀ-ÿs]{10,}$",
@@ -50,19 +71,61 @@ const submitContact = (e) => {
   );
 
   // checked button validation
-  if (!inputConditions.checked)
+  if (!inputConditions.checked) {
     inputConditions.nextElementSibling.classList.add("text-red-400");
-  else inputConditions.nextElementSibling.classList.remove("text-red-400");
+    isValid = false;
+  } else {
+    inputConditions.nextElementSibling.classList.remove("text-red-400");
+  }
 
   // radio validation
   if (!optionMmeCivilite.checked && !optionMCivilite.checked) {
     optionMmeCivilite.nextElementSibling.classList.add("text-red-400");
     optionMCivilite.nextElementSibling.classList.add("text-red-400");
+    isValid = false;
   } else {
     optionMmeCivilite.nextElementSibling.classList.remove("text-red-400");
     optionMCivilite.nextElementSibling.classList.remove("text-red-400");
   }
+
+  console.log(isValid);
+  if (isValid) {
+    showToast("Form details have been sent successfully.", "success");
+  } else {
+    showToast("Failed to send form details.", "error");
+  }
 };
+
+function showToast(message, type) {
+  const toastContainer = document.getElementById("toast-container");
+
+  // Remove any existing toast before adding a new one
+  const existingToast = document.querySelector("custom-toast");
+  if (existingToast) {
+    existingToast.remove();
+  }
+
+  // Create a new toast
+  const toastDiv = document.createElement("custom-toast");
+  toastDiv.setAttribute("message", message);
+  toastDiv.setAttribute("type", type);
+
+  toastContainer.appendChild(toastDiv);
+
+  // to wait the toastDiv to be created in the dom
+  setTimeout(() => {
+    const toast = toastDiv.querySelector("#toast");
+    if (toast) {
+      toast.classList.remove("-translate-y-[100px]");
+    }
+  }, 1);
+
+  setTimeout(() => {
+    if (toastDiv) {
+      toastDiv.querySelector("#toast").classList.add("-translate-y-[100px]");
+    }
+  }, 2000);
+}
 
 const btn = document.getElementById("submit-form");
 btn.addEventListener("click", submitContact);
